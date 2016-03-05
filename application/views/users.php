d15b8fdf690de428b74dde5e642ba85d387b83fe
Epsvictor <vicluna003@gmail.com>
2016-03-04 23:01:49 -0600

    CRUD version 1

---
+++ application/views/users.php
@@ -0,0 +1,36 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<style type="text/css">
+	h1 {
+		color: #D51818;
+		background-color: transparent;
+		border-bottom: 5px solid #AB0CA0;
+		font-size: 44px;
+		font-weight: normal;
+		margin: 0 0 14px 0;
+		padding: 14px 15px 10px 15px;
+	}
+
+	h2 {
+ 		color: #D51818;
+		background-color: transparent;
+		font-size: 20px;
+		font-weight: bold;
+	}
+
+
+</style>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CRUDELIO</title>
+</head>
+<body>
+  <h1>BIENVENIDO AL CRUD</h1>
+   <h2>  Puedes checar el codigo <a href="https://github.com/Epsvictor/CRUD" target="blank">en GitHub</a></h2>
+</body>
+</html>
+
+
